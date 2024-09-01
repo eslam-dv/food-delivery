b218e4dcfe2dbe3f1445b63aac4be99ed7616f22
Eslam Osama <eslamebrahim1301@gmail.com>
2024-09-01 16:32:33 +0300

    deploy to vercel

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
-const uri = process.env.MONGO_URI
+const uri = `${process.env.MONGO_URI}`;
 
 export const connectDB = async () => {
-  await mongoose.connect(uri).then(() => console.log("DB Connected"))
-}
-
+	await mongoose.connect(uri).then(() => console.log("DB Connected"));
+};
